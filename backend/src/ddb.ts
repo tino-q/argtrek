825c3a0aab938aed3494ba07ab30f29a521303f5
martin <martin.queija@didomi.io>
2025-09-16 10:21:53 +0200

    ddb

---
+++ backend/src/ddb.ts
@@ -0,0 +1,40 @@
+// implement a minimal dynamo db client to get and set string items
+
+import { config } from "dotenv";
+import {
+  DynamoDBClient,
+  GetItemCommand,
+  PutItemCommand,
+} from "@aws-sdk/client-dynamodb";
+
+config({ path: ".env" });
+
+const ddb = new DynamoDBClient({
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: process.env["AWS_ACCESS_KEY_ID"] as string,
+    secretAccessKey: process.env["AWS_SECRET_ACCESS_KEY"] as string,
+  },
+});
+
+export const getItem = async (key: string) => {
+  const command = new GetItemCommand({
+    TableName: "ArgTripCache",
+    Key: { key: { S: key } },
+  });
+  const response = await ddb.send(command);
+  return response.Item?.["value"]?.S;
+};
+
+export const setItem = async (key: string, value: string, ttlHours = 4) => {
+  const ttlTimestamp = Math.floor(Date.now() / 1000) + ttlHours * 3600;
+  const command = new PutItemCommand({
+    TableName: "ArgTripCache",
+    Item: {
+      key: { S: key },
+      value: { S: value },
+      ttl: { N: ttlTimestamp.toString() },
+    },
+  });
+  await ddb.send(command);
+};
