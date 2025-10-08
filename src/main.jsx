3e0fd8f22caadd882e2adca5a00446ba3ee288ac
martin <martin.queija@didomi.io>
2025-10-08 12:29:25 +0200

    cont

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { setupGlobalErrorHandlers } from "./utils/errorReporting.js";
 
 // One-time localStorage reset for backward compatibility
 (function resetLocalStorageOnce() {
-  const resetKey = "reset1";
+  const resetKey = "reset2";
   const hasReset = localStorage.getItem(resetKey);
 
   if (!hasReset) {
